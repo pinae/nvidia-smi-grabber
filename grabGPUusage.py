71a3a8cea57f10ad679185516231743650401906
pinae <jonny@pinae.net>
2016-09-28 10:46:48 +0200

    Grab the GPU usage percentage from nvidia-smi and collect the value 20 times per second

---
+++ grabGPUusage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+import subprocess
+import time
+
+values = []
+try:
+    while True:
+        p = subprocess.Popen(["nvidia-smi"], stdout=subprocess.PIPE, shell=True)
+        out, err = p.communicate()
+        gpu_usage = int(out.splitlines()[8][56:].split(b"%")[0])
+        values.append(gpu_usage)
+        print("GPU usage {:2.1%}".format(gpu_usage / 100), end="\r")
+        time.sleep(0.005)
+except KeyboardInterrupt:
+    print("")
+    print("captured values:")
+    print(", ".join([str(v) for v in values]))
